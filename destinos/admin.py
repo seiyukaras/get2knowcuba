6f0314db961a012d9578f798bb7f3ab37df48ff9
s384 Torre <seba.alexis.trujillo@gmail.com>
2020-02-19 20:47:20 -0300

    Include or not include

--- destinos/admin.py
+++ destinos/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Paquete
+from .models import Paquete, ServiciosInlcuidos, ServiciosNoInlcuidos
 
 class PaqueteAdmin(admin.ModelAdmin):
      prepopulated_fields = { 'slug': ['titulo']}
 
 admin.site.register(Paquete, PaqueteAdmin)
+admin.site.register(ServiciosInlcuidos)
+admin.site.register(ServiciosNoInlcuidos)
--- destinos/models.py
+++ destinos/models.py
@@ -2,6 +2,29 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 # Create your models here.
+class ServiciosInlcuidos(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name="Servicio incluido"
+        verbose_name_plural="Servicios incluidos"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+
+
+class ServiciosNoInlcuidos(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name="Servicio No incluido"
+        verbose_name_plural="Servicios No incluidos"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+
 
 class Paquete(models.Model):
     titulo = models.CharField(max_length=200)
@@ -17,6 +40,9 @@ class Paquete(models.Model):
     home = models.BooleanField(default=False)
     round = models.BooleanField(default=False)
     oferta = models.BooleanField(default=False)
+    servicios = models.ManyToManyField(ServiciosInlcuidos, verbose_name="Servicios Inlcuidos", related_name="get_services")
+    noservicios = models.ManyToManyField(ServiciosNoInlcuidos, verbose_name="Servicios No Inlcuidos", related_name="get_not_services")
 
     def __str__(self):
         return self.titulo
+
